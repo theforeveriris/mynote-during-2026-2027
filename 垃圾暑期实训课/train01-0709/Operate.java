d98d362511f4e47e449c5c32b5d3895dae7800cb
theforeveriris <theforeveriris@proton.me>
2026-08-02 14:26:24 +0800

    chore: 移除校园电竞社团管理系统项目及相关图片文件,补全网际协议笔记
    清理本次提交中所有的课程项目源码、文档和配套图片文件，移除整个train01-0709项目目录相关内容

--- 垃圾暑期实训课/train01-0709/Operate.java
+++
@@ -1,9 +0,0 @@
-/*定义公共接口 Operate */
-public interface Operate {
-    /*创建比赛对局，参数为两队ID和比赛结果，无返回值 */
-    void createMatch(int teamAId, int teamBId, String result);
-    /*按游戏分类统计积分，无参数无返回值 */
-    void statScoreByGame();
-    /*声明抽象方法：拉黑选手，参数为选手ID，无返回值 */
-    void blacklistPlayer(int playerId);
-}
